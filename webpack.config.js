bd5579bc230fb0a87fc92bf35f9a76de16d73229
Greg Schneider <greg53x@msn.com>
2023-03-10 22:39:19 -0700

    webpack mode to development and add inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',  
   entry: {
       index: './src/index.js',
       tab1: './src/tab1.js',
   },
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
