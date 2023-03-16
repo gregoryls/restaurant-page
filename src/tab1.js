bf1c1ba0592b5a18b36521bb5bf6628db83d12e0
Greg Schneider <greg53x@msn.com>
2023-03-16 13:26:52 -0600

    alter create element function to ignore id if not supplied

--- src/tab1.js
+++ src/tab1.js
@@ -1,8 +1,10 @@
 export function createElementWithId(element, id) {
   const elem = document.createElement(element);
+  if (id === undefined) return elem;
   elem.id = id;
   return elem;
 }
+// TODO put review inside a p probably
 
 function createReview() {
   const review = createElementWithId("div", "review");
@@ -10,7 +12,15 @@ function createReview() {
   return review;
 }
 
+function createHours() {
+  const hours = createElementWithId("div", "hours");
+  const hoursList = createElementWithId("ol");
+  hours.appendChild(hoursList);
+  return hours;
+}
+
 export default function tab1() {
   const content = document.getElementById("content");
   content.appendChild(createReview());
+  content.appendChild(createHours());
 }
