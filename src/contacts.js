7b0cd61300b182d1dc29b059b3fc2fbc0d092a4d
Greg Schneider <greg53x@msn.com>
2023-03-21 23:10:51 -0600

    set up contact module to append onto #content div

--- src/contacts.js
+++ src/contacts.js
@@ -1,3 +1,15 @@
+import { createElementWithId } from "./tab1";
+
+function contact(name, role, phone, email) {
+  const contactInfo = createElementWithId("div");
+  const contactName = createElementWithId("h3");
+  contactName.textContent = name;
+  contactInfo.appendChild(contactName);
+
+  return contactInfo;
+}
+
 export default function contactPage() {
-  console.log("testcontact");
+  const content = document.getElementById("content");
+  content.appendChild(contact("john"));
 }
