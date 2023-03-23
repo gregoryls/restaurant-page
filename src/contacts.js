c3007f514ed7e13c8a7f9f58a3e4b7292e724052
Greg Schneider <greg53x@msn.com>
2023-03-22 21:37:20 -0600

    build contact module to display name, role, phone, and email

--- src/contacts.js
+++ src/contacts.js
@@ -2,14 +2,35 @@ import { createElementWithId } from "./tab1";
 
 function contact(name, role, phone, email) {
   const contactInfo = createElementWithId("div");
+
   const contactName = createElementWithId("h3");
   contactName.textContent = name;
   contactInfo.appendChild(contactName);
+
+  const roleTitle = createElementWithId("p");
+  roleTitle.textContent = role;
+  contactInfo.appendChild(roleTitle);
+
+  const phoneNumber = createElementWithId("p");
+  phoneNumber.textContent = phone;
+  contactInfo.appendChild(phoneNumber);
+
+  const emailAddress = createElementWithId("p");
+  emailAddress.textContent = email;
+  contactInfo.appendChild(emailAddress);
 
   return contactInfo;
 }
 
 export default function contactPage() {
   const content = document.getElementById("content");
-  content.appendChild(contact("john"));
+  content.appendChild(
+    contact("Jane", "Owner", "111-222-3333", "owner@restaurant.com")
+  );
+  content.appendChild(
+    contact("john", "Chef", "888-777-6666", "chef@restaurant.com")
+  );
+  content.appendChild(
+    contact("Sam", "Waiter", "444-555-6666", "waiter@restaurant.com")
+  );
 }
