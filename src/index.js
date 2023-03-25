864dbe3fbc5f76639358383644172235bcc011c0
Greg Schneider <greg53x@msn.com>
2023-03-24 23:16:07 -0600

    add a restaurant name below splash header and above content

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import menu from "./menu";
 import contactPage from "./contacts";
 
 document.body.appendChild(importImage(Splash));
+
+const title = createElementWithId("h1", "title");
+title.textContent = "Farm2Table";
+document.body.appendChild(title);
+
 document.body.appendChild(createElementWithId("div", "content"));
 
 tab1();
