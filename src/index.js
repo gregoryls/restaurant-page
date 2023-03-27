6c7f9fa712b2f6afeeb23ae2fa2a538e99e6b091
Greg Schneider <greg53x@msn.com>
2023-03-26 23:25:42 -0600

    add site navigation buttons

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import tab1, { createElementWithId, importImage } from "./home";
 import menu from "./menu";
 import contactPage from "./contacts";
 
+function createTabButtons() {
+  const homeButton = createElementWithId("button", "homeButton");
+  homeButton.textContent = "Home";
+  document.body.appendChild(homeButton);
+
+  const menuButton = createElementWithId("button", "menuButton");
+  menuButton.textContent = "Menu";
+  document.body.appendChild(menuButton);
+
+  const contactButton = createElementWithId("button", "contactButton");
+  contactButton.textContent = "Contact Us";
+  document.body.appendChild(contactButton);
+}
+
 document.body.appendChild(importImage(Splash));
 
 const title = createElementWithId("h1", "title");
 title.textContent = "Farm2Table";
 document.body.appendChild(title);
+
+createTabButtons();
 
 document.body.appendChild(createElementWithId("div", "content"));
 
