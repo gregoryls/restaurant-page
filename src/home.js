c0049d48e37ec5f541ffb51c6e4774917a5eec1c
Greg Schneider <greg53x@msn.com>
2023-04-08 23:09:33 -0600

    put customer review into a dedicated variable

--- src/home.js
+++ src/home.js
@@ -13,10 +13,14 @@ export function importImage(source) {
 }
 
 function createReview() {
+  // creates a div with a restaurant review followed by the name of the reviewer
+  const customerReview =
+    "I recently had the pleasure of dining at a farm to table restaurant, and I must say, it was an outstanding experience. From the moment I walked in, the atmosphere was warm and inviting, with a charming rustic decor and a cozy feel. The menu was full of fresh, locally-sourced ingredients, and each dish was prepared with skill and care.";
+
   const review = createElementWithId("div", "review");
   const reviewText = createElementWithId("p", "reviewText");
-  reviewText.textContent =
-    "I recently had the pleasure of dining at a farm to table restaurant, and I must say, it was an outstanding experience. From the moment I walked in, the atmosphere was warm and inviting, with a charming rustic decor and a cozy feel. The menu was full of fresh, locally-sourced ingredients, and each dish was prepared with skill and care.";
+  reviewText.textContent = customerReview;
+
   const reviewName = createElementWithId("p", "reviewName");
   reviewName.textContent = "- John Eater";
   review.appendChild(reviewText);
